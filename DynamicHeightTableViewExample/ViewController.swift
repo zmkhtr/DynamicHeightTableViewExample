3f0ed664b2821a4dd031cfbe15a227ee17fb22d5
zmkhtr <azammukhtar3@gmail.com>
2023-03-04 16:14:30 +0700

    Add example

--- DynamicHeightTableViewExample/ViewController.swift
+++ DynamicHeightTableViewExample/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExampleCell", for: indexPath) as! ExampleCell
+        
+        let myAttributes = [ NSAttributedString.Key.foregroundColor: UIColor.blue ]
+        let myString = NSMutableAttributedString(string: randomString(length: Int.random(in: 100..<1000)), attributes: myAttributes )
 
+        cell.bodyTextView.attributedText = myString
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    private func randomString(length: Int) -> String {
+      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+      return String((0..<length).map{ _ in letters.randomElement()! })
+    }
 }
 
+class ExampleCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    
+}
